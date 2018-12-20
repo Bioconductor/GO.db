d40ae4b9f776979b71159cf8c3523bd5903ef152
lshep <lori.shepherd@roswellpark.org>
2018-12-20 08:01:17 -0500

    Add initial metadata.csv and make-data scripts
      - scripts needed to convert to AnnotationHub

---
+++ inst/scripts/make-data.R
@@ -0,0 +1,4 @@
+## The Data obtained from The Gene Ontology Consortium is downloaded and
+## processed using the BioConductorAnnotationPipeline package in this
+## GitHub repository:
+# browseURL("https://github.com/Bioconductor/BioconductorAnnotationPipeline")
