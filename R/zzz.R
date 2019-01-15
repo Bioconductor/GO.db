f9cb3343b9edbba50adcaaba62d81993f24c564f
Daniel Van Twisk <daniel.vantwisk@roswellpark.org>
2019-01-15 10:46:54 -0500

    Further changes to zzz.r

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 datacache <- new.env(hash=TRUE, parent=emptyenv())
 
 GO <- function() showQCData("GO", datacache)
-GO_dbconn <- function() dbconn(datacache)
+GO_dbconn <- function() dbFileConnect(GO_dbfile())
 GO_dbfile <- function() dbfile(datacache)
 GO_dbschema <- function(file="", show.indices=FALSE) dbschema(datacache, file=file, show.indices=show.indices)
 GO_dbInfo <- function() dbInfo(datacache)
@@ -9,32 +9,25 @@ GO_dbInfo <- function() dbInfo(datacache)
 .onLoad <- function(libname, pkgname)
 {
     ## Connect to the SQLite DB
-    assign("dbfile", dbfile, envir=datacache)
     delayedAssign("dbfile", {
         cache(AnnotationHub()["AH67900"])
-    }, assign.env = datacache)
-    delayedAssign("dbconn", {
-        dbFileConnect(dbfile)
     }, assign.env = datacache)
 
     ## Create the OrgDb object
     sPkgname <- sub(".db$","",pkgname)
-    delayedAssign("txdb", {
-        loadDb(dbfile)
-    }, assign.env = datacache)
     dbNewname <- AnnotationDbi:::dbObjectName(pkgname,"GODb")
     ns <- asNamespace(pkgname)
     delayedAssign(dbNewname, {
-        txdb
+        loadDb(GO_dbfile())
     }, assign.env = ns)
     namespaceExport(ns, dbNewname)
 
     ## Create the AnnObj instances
     delayedAssign("ann_objs", {
-        ann_objs <- createAnnObjs.SchemaChoice("GO_DB", "GO", "GO", dbconn, datacache)
-        mergeToNamespaceAndExport(ann_objs, pkgname)
+        ann_objs <- createAnnObjs.SchemaChoice("GO_DB", "GO", "GO", GO_dbconn(), datacache)
         ann_objs
     }, assign.env = datacache)
+    NamespaceAndExport(ann_objs, pkgname)
     packageStartupMessage(AnnotationDbi:::annoStartupMessages("GO.db"))
 }
 
